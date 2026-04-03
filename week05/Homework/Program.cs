8a34d4136827b117ed220a60190c21e6f5a2e165
ekimball1 <ekimball1@byupathway.edu>
2026-04-03 18:27:53 -0500

    W05_Homework_Project

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        MathAssignment math = new MathAssignment(
+            "Liana Lopez",
+            "Division",
+            "2.1",
+            "1-10"
+        );
+
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+
+        Console.WriteLine();
+
+        WritingAssignment writing = new WritingAssignment(
+            "Aldo Walten",
+            "History 101",
+            "The Effects of the Cold War"
+        );
+
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
     }
 }
